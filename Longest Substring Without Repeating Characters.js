3805639c32fb02e264531096def1105b8b911018
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-10-14 15:43:09 -0700

    Add files via upload

---
+++ Longest Substring Without Repeating Characters.js
@@ -0,0 +1,36 @@
+/*
+Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.*/ 
+
+const lengthOfLongestSubstring = s => {
+  const map = {};
+  let left = 0;
+  return s.split('').reduce((max, char, index) => {
+    left = map[char] >= left ? map[char] + 1 : left;
+    map[char] = index;
+    const window = index - left + 1;
+    return max = Math.max(max, window);
+
+  },0)
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb")); 
+console.log(lengthOfLongestSubstring("pwwkew"));
---
+++ Valid Palindrome.js
@@ -0,0 +1,49 @@
+/*
+https://leetcode.com/problems/valid-palindrome/
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isPalindrome = function(s) {
+  s = s.toLowerCase().replace(/[_\W]/gi,'');
+  let start = 0;
+  let end = s.length - 1;
+  
+  while(start < end) {
+      if (s[start] !== s[end]) {
+          return false
+      }
+      start ++;
+      end --;
+  }
+  return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("race a car"));
+console.log(isPalindrome(" "));
